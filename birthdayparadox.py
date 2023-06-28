28c71092a05e78b7d6c3d6c0f20efa86d3bdbb27
irishandyesiam <nathan.irish@gmail.com>
2023-06-27 23:40:27 -0500

    Added function getBirthdays.

--- birthdayparadox.py
+++ birthdayparadox.py
@@ -3,3 +3,20 @@ Explore the suprising probabilities of the "Birthday Paradox".
 More info at https://en.wikipedia.org/wiki/Birthday_problem
 This code is available at https://nostarch.com/big-book-small-python-programming
 Tags: short, math, simulation"""
+
+import datetime, random
+
+
+def getBirthdays(numberOfBirthdays):
+    """Returns a list of number random date objects for birthdays."""
+    birthdays = []
+    for i in range(numberOfBirthdays):
+        # The year is unimportant for our simulation, as long as all
+        # birthdays have the same year.
+        startOfYear = datetime.date(2001, 1, 1)
+
+        # Get a random day into the year:
+        randomNumberOfDays = datetime.timedelta(random.randint(0, 364))
+        birthday = startOfYear + randomNumberOfDays
+        birthdays.append(birthday)
+    return birthdays
