909d41998d2abad5db024e485c50ab4fdcf5fb1f
irishandyesiam <nathan.irish@gmail.com>
2023-06-27 23:35:03 -0500

    Opened file for birthday paradox with Intro.

---
+++ birthdayparadox.py
@@ -0,0 +1,5 @@
+"""Birthday Paradox Simulation, by Al Sweigart al@inventwithpython.com
+Explore the suprising probabilities of the "Birthday Paradox".
+More info at https://en.wikipedia.org/wiki/Birthday_problem
+This code is available at https://nostarch.com/big-book-small-python-programming
+Tags: short, math, simulation"""
